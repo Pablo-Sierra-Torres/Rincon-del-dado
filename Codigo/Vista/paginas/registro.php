f6df30d91b6969982aa6526605cc8b385b2004fa
Franciskis <63367727+FranDCN@users.noreply.github.com>
2022-05-21 16:19:25 +0200

    rutas

--- Codigo/Vista/paginas/registro.php
+++ Codigo/Vista/paginas/registro.php
@@ -8,17 +8,17 @@
        
         <meta charset="UTF-8">
 
-        <link rel="stylesheet" type="text/css" href="../../Vista/bootstrap/css/bootstrap.min.css">
-		    <script src="../../Vista/bootstrap/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" type="text/css" href="../Vista/bootstrap/css/bootstrap.min.css">
+		    <script src="../Vista/bootstrap/js/bootstrap.min.js"></script>
 
-        <link rel="stylesheet" href="../../Vista/css/var.css">
-        <link rel="stylesheet" href="../../Vista/css/cabecera.css">
-        <link rel="stylesheet" href="../../Vista/css/footer.css">
-        <link rel="stylesheet" href="../../Vista/css/registro.css">
+        <link rel="stylesheet" href="../Vista/css/var.css">
+        <link rel="stylesheet" href="../Vista/css/cabecera.css">
+        <link rel="stylesheet" href="../Vista/css/footer.css">
+        <link rel="stylesheet" href="../Vista/css/registro.css">
         
         <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
-        <script defer src="../../Vista/javascript/slider.js"></script>
-        <script defer src="../../Vista/javascript/dados.js"></script>
+        <script defer src="../Vista/javascript/slider.js"></script>
+        <script defer src="../Vista/javascript/dados.js"></script>
 
 
     </head>
