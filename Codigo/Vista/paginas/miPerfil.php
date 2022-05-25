4cff77256a7863d2e1b7b6c58f0d8dad56174972
Franciskis <63367727+FranDCN@users.noreply.github.com>
2022-05-25 13:46:55 +0200

    mi Perfil termiinado

--- Codigo/Vista/paginas/miPerfil.php
+++ Codigo/Vista/paginas/miPerfil.php
@@ -8,18 +8,17 @@
        
         <meta charset="UTF-8">
 
-        <link rel="stylesheet" type="text/css" href="../Vista/bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="../bootstrap/css/bootstrap.min.css">
 		    <script src="../Vista/bootstrap/js/bootstrap.min.js"></script>
 
-        <link rel="stylesheet" href="../Vista/css/var.css">
-        <link rel="stylesheet" href="../Vista/css/cabecera.css">
-        <link rel="stylesheet" href="../Vista/css/footer.css">
-        <link rel="stylesheet" href="../Vista/css/miPerfil.css">
+        <link rel="stylesheet" href="../css/var.css">
+        <link rel="stylesheet" href="../css/cabecera.css">
+        <link rel="stylesheet" href="../css/footer.css">
+        <link rel="stylesheet" href="../css/miPerfil.css">
         
         <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
-        <script defer src="../Vista/javascript/slider.js"></script>
-        <script defer src="../Vista/javascript/dados.js"></script>
-
+        <script defer src="../javascript/slider.js"></script>
+        <script defer src="../javascript/dados.js"></script>
 
     </head>
 
@@ -27,12 +26,12 @@
 
       <!--CABECERA-->
 
-      <?php 
+      <?php /*
         if(validarSesion()){
           include '../Vista/partes/cabeceraLogeado.php';
         }else{
-          include '../Vista/partes/cabecera.php';
-        }
+          */include '../partes/cabecera.php';
+        //}
       ?>
       
       <!--CUERPO-->
@@ -43,7 +42,7 @@
 
           <div class="col-lg-12" id="">
 
-            <h2>Parte 1</h2>    
+            <br>    <br>
 
             <div class="row">  
                 <div class="col-lg-2"></div> 
@@ -60,23 +59,81 @@
                         <div class="profile-img" style="width: 250px; height: 250px; background-size: cover; background-position: center; background-image: url('/<?= $url ?>');">
                         </div>*/
                     ?>
-                        <img src="../Vista/imgs/usuarios/default.png" class="imgPerfil" alt="...">
+                        <img src="../imgs/usuarios/default.png" class="imgPerfil" alt="...">
+                        <br><br>
+                        <button type="submit" class="btn btn-danger" name="subir">sube nueva imagen de perfil</button>
                     </div>
                 </div>
-                <div class="col-lg-3">  
-                    <div class=" input-group mb-3">
-                        <br>
-                        <h4><?php echo $datos->Nombre;?></h4>
-                        <br><br>
-                        <h6>descripción super descriptiva</h6>
+                <div class="col-lg-5">  
+                    <div class="row">  
+                        <div class="col-lg-12">
+                            <h4>Nombre super nombrado<?php //echo $datos->Nombre;?></h4>
+                        </div>
+                    </div>
+                    <div class="row">        
+                        <div class="col-lg-12">      
+                            <h6>descripción super descriptiva</h6>
+                        </div>    
                     </div>
                 </div>  
-                <div class="col-lg-4"></div>  
             </div>
-            <h2>Parte 2</h2>            
 
-          </div>
+            <br><br> 
+            
+            <div class="row">
+                <div class="col-lg-2"></div>   
+                <div class="col-lg-4">
+                    <h4>Datos de contacto</h4>
+                </div>
+                <div class="col-lg-8"></div>
+            </div>     
+            <div class="row">
+                <div class="col-lg-3"></div>   
+                <div class="col-lg-5">
+                    <h5>Email : mail@mail.com</h5>
+                </div>
+            </div>    
+            <div class="row">
+                <div class="col-lg-3"></div>   
+                <div class="col-lg-5">
+                    <h5>T.Fijo : 915555555</h5>
+                </div>
+                </div>    
+            <div class="row">
+                <div class="col-lg-3"></div>    
+                <div class="col-lg-5">
+                    <h5>T.Movil : 605555555</h5>
+                </div>
+            </div>
 
+            <div class="row">
+                <div class="col-lg-2"></div>   
+                <div class="col-lg-4">
+                    <h4>Datos de envío</h4>
+                </div>
+                <div class="col-lg-8"></div>
+            </div>     
+            <div class="row">
+                <div class="col-lg-3"></div>   
+                <div class="col-lg-5">
+                    <h5>Dirección : C/ de la Imaginacion 21 5º A</h5>
+                </div>
+            </div>    
+            <div class="row">
+                <div class="col-lg-3"></div>   
+                <div class="col-lg-5">
+                    <h5>Población : Madrid</h5>
+                </div>
+            </div>    
+            <div class="row">
+                <div class="col-lg-3"></div>    
+                <div class="col-lg-5">
+                    <h5>Código postal : 28000</h5>
+                </div>
+            </div>
+            
+            </div>
+            
         </div>
 
       </div>
@@ -88,7 +145,7 @@
      
       <!--FOOTER-->
       
-      <?php include '../Vista/partes/footer.php';?> 
+      <?php include '../partes/footer.php';?> 
 
     </body>
 
