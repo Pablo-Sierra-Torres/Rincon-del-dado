435f51221d4047f25ce866a52611ebf1c0b75b51
Pablo-Sierra-Torres <63937863+Pablo-Sierra-Torres@users.noreply.github.com>
2022-05-29 16:25:56 +0200

    Update tienda.js

---
+++ Codigo/Vista/javascript/tienda.js
@@ -0,0 +1,21 @@
+function añadeFuncionalidadBotonCompra(){
+
+    let botonesComprar = document.getElementsByTagName("botonCompra");
+
+    for (let i = 0; i < celdas.length; i++) {
+        botonesComprar[i].addEventListener('click',añadeCarro);
+    }
+}
+
+let Carro = [];
+
+function añadeCarro(evt){
+
+
+    this.id
+
+
+
+}
+
+function muestraCarro()
