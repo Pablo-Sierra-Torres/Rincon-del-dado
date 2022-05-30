6e86a05175b4dd3260dd3ab6df445f8ccbf48e51
Juan Carlos Tauroni Roldán <juanctauronir@gmail.com>
2022-05-30 19:59:46 +0200

    cambios, faltan

---
+++ Codigo/Vista/javascript/realizarCompra.js
@@ -0,0 +1,11 @@
+$('#id-btn-pago').click(function (e) { 
+    e.preventDefault();
+    $(`<div class="card" id="id-conf-compra">
+       <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <h6 class="card-subtitle mb-2 text-muted ">Card subtitle</h6>
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      b5
+    </div>
+  </div>`).appendTo('body');
+});
