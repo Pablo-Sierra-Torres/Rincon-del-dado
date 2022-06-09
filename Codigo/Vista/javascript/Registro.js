57169f676b9c5733c5cb602765eebe6cd1c9a4f4
Pablo-Sierra-Torres <63937863+Pablo-Sierra-Torres@users.noreply.github.com>
2022-06-09 22:50:11 +0200

    registro

---
+++ Codigo/Vista/javascript/Registro.js
@@ -0,0 +1,96 @@
+
+let formulario = document.getElementById("formformularioRegistroularioLogin");
+let boton = document.getElementById("botonenviar");
+
+var correo;
+var pass;
+
+//*añadir ID a los input y al boton en html, añadir required
+
+
+formulario.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    correo = document.getElementById("correoInput");
+    pass = document.getElementById("passInput");
+
+    if (validar()) {
+        this.submit();
+            /*
+            comprobacion de php
+            */
+    }
+
+}, false);
+
+function validar() {
+    campos = [correo, pass]
+    for (let i = 0; i < campos.length; i++) {
+
+        campos[i].setCustomValidity(''); //no deja el mensaje vacio sino el mensaje generico "Completa este campo"
+    }
+
+    return (validarAPI() && validarJS())
+
+}
+
+function validarJS() {
+    return correoJS() && passJS();
+}
+
+function validarAPI() {
+    return correoAPI() && passAPI();
+
+}
+
+
+function correoAPI() {
+
+    if (correo.validity.valueMissing) {
+        correo.setCustomValidity("Introduce tu correo electronico");
+        return false;
+    } else if (correo.validity.patternMissmatch) {
+
+        correo.setCustomValidity("Direccion de correo no valida api");
+        return false;
+    } else {
+        return true;
+    }
+}
+
+//comprobar esto
+function correoJS() {
+    let correoValor = correo.value;
+    let patron = /^[^@]+@[^@]+\.[a-zA-Z]{2,}$/;
+
+    if (!patron.test(correoValor)) {
+        correo.setCustomValidity("Direccion de correo no valido js ");
+        return false
+    } else {
+        return true;
+    }
+
+
+}
+
+function passAPI() {
+    if (pass.validity.valueMissing) {
+        pass.setCustomValidity("Hace falta contraseña");
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function passJS() {
+
+    let passValor = pass.value;
+
+    if (passValor == '') {
+
+        pass.setCustomValidity("Hace falta contraseña");
+        return false;
+    } else {
+        return true;
+    }
+}
--- Codigo/Vista/paginas/registro.php
+++ Codigo/Vista/paginas/registro.php
@@ -43,7 +43,7 @@
             <p>¿Ya eres miembro? <a href="ControladorLogin.php" class="enlace">Inicia Sesión</a></p>
             <hr>
 
-            <form method="POST" action="ControladorRegister.php">
+            <form id="formularioRegistro"method="POST" action="ControladorRegister.php">
             <div class="row">  
                 <div class="col-lg-6">  
                     <h5>Nombre</h5>
@@ -52,7 +52,7 @@
                             <path d="M1.5 1a.5.5 0 0 0-.5.5v3a.5.5 0 0 1-1 0v-3A1.5 1.5 0 0 1 1.5 0h3a.5.5 0 0 1 0 1h-3zM11 .5a.5.5 0 0 1 .5-.5h3A1.5 1.5 0 0 1 16 1.5v3a.5.5 0 0 1-1 0v-3a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 1-.5-.5zM.5 11a.5.5 0 0 1 .5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 1 0 1h-3A1.5 1.5 0 0 1 0 14.5v-3a.5.5 0 0 1 .5-.5zm15 0a.5.5 0 0 1 .5.5v3a1.5 1.5 0 0 1-1.5 1.5h-3a.5.5 0 0 1 0-1h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 1 .5-.5z"/>
                             <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm8-9a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
                         </svg></span>
-                        <input type="text" class="form-control" placeholder="Pedro" aria-label="Username" aria-describedby="basic-addon1" name="nombre">
+                        <input id="nombre" type="text" class="form-control" placeholder="Pedro" aria-label="Username" aria-describedby="basic-addon1" name="nombre" required>
                     </div>
                 </div>
                 <div class="col-lg-6">  
@@ -62,7 +62,7 @@
                             <path d="M1.5 1a.5.5 0 0 0-.5.5v3a.5.5 0 0 1-1 0v-3A1.5 1.5 0 0 1 1.5 0h3a.5.5 0 0 1 0 1h-3zM11 .5a.5.5 0 0 1 .5-.5h3A1.5 1.5 0 0 1 16 1.5v3a.5.5 0 0 1-1 0v-3a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 1-.5-.5zM.5 11a.5.5 0 0 1 .5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 1 0 1h-3A1.5 1.5 0 0 1 0 14.5v-3a.5.5 0 0 1 .5-.5zm15 0a.5.5 0 0 1 .5.5v3a1.5 1.5 0 0 1-1.5 1.5h-3a.5.5 0 0 1 0-1h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 1 .5-.5z"/>
                             <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm8-9a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
                         </svg></span>
-                        <input type="text" class="form-control" placeholder="Cordillera" aria-label="Username" aria-describedby="basic-addon1" nombre ="apellidos">
+                        <input id="apellidos" type="text" class="form-control" placeholder="Cordillera" aria-label="Username" aria-describedby="basic-addon1" nombre ="apellidos" required>
                     </div>
                 </div>   
                 </div>     
@@ -73,7 +73,7 @@
                     <span class="input-group-text" id="basic-addon1"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope icol" viewBox="0 0 16 16">
                     <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z"/>
                     </svg></span>
-                    <input type="text" class="form-control" placeholder="ejemplo@gmail.com" aria-label="Username" aria-describedby="basic-addon1" name="correo">
+                    <input id="correo" type="text" class="form-control" placeholder="ejemplo@gmail.com" aria-label="Username" aria-describedby="basic-addon1" name="correo"  pattern="^[^@]+@[^@]+\.[a-zA-Z]{2,}$" required>
                 </div>
               </div>
               <div class="col-lg-6">  
@@ -82,7 +82,7 @@
                     <span class="input-group-text" id="basic-addon1"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope icol" viewBox="0 0 16 16">
                     <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z"/>
                     </svg></span>
-                    <input type="text" class="form-control" placeholder="ejemplo@gmail.com" aria-label="Username" aria-describedby="basic-addon1" name="confirmar_correo">
+                    <input id="correo2"type="text" class="form-control" placeholder="ejemplo@gmail.com" aria-label="Username" aria-describedby="basic-addon1" name="confirmar_correo"  pattern="^[^@]+@[^@]+\.[a-zA-Z]{2,}$" required>
                 </div>
               </div>   
             </div> 
@@ -93,7 +93,7 @@
                     <span class="input-group-text" id="basic-addon2"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-key-fill icol" viewBox="0 0 16 16">
                     <path d="M3.5 11.5a3.5 3.5 0 1 1 3.163-5H14L15.5 8 14 9.5l-1-1-1 1-1-1-1 1-1-1-1 1H6.663a3.5 3.5 0 0 1-3.163 2zM2.5 9a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"/>
                     </svg></span>
-                    <input type="password" class="form-control" placeholder="" aria-label="Username" aria-describedby="basic-addon1" name="passd">
+                    <input id="pass" type="password" class="form-control" placeholder="" aria-label="Username" aria-describedby="basic-addon1" name="passd">
                     
                 </div>
               </div>  
@@ -103,7 +103,7 @@
                     <span class="input-group-text" id="basic-addon2"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-key-fill icol" viewBox="0 0 16 16">
                     <path d="M3.5 11.5a3.5 3.5 0 1 1 3.163-5H14L15.5 8 14 9.5l-1-1-1 1-1-1-1 1-1-1-1 1H6.663a3.5 3.5 0 0 1-3.163 2zM2.5 9a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"/>
                     </svg></span>
-                    <input type="password" class="form-control" placeholder="" aria-label="Username" aria-describedby="basic-addon1" name="confirmar_passd">
+                    <input id="pass2" type="password" class="form-control" placeholder="" aria-label="Username" aria-describedby="basic-addon1" name="confirmar_passd">
                     
                 </div>
               </div> 
@@ -116,7 +116,7 @@
                       <path d="M9 8.5h2.793l.853.854A.5.5 0 0 0 13 9.5h1a.5.5 0 0 0 .5-.5V8a.5.5 0 0 0-.5-.5H9v1z"/>
                       <path d="M12 3H4a4 4 0 0 0-4 4v6a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1V7a4 4 0 0 0-4-4zM8 7a3.99 3.99 0 0 0-1.354-3H12a3 3 0 0 1 3 3v6H8V7zm-3.415.157C4.42 7.087 4.218 7 4 7c-.218 0-.42.086-.585.157C3.164 7.264 3 7.334 3 7a1 1 0 0 1 2 0c0 .334-.164.264-.415.157z"/>
                     </svg></span>
-                    <input type="text" class="form-control" placeholder="28000" aria-label="Username" aria-describedby="basic-addon1" name="codigo_postal">
+                    <input id="cp" type="text" class="form-control" placeholder="28000" aria-label="Username" aria-describedby="basic-addon1" name="codigo_postal" pattern="^\d{5}$" required>
                 </div>
               </div>
               <div class="col-lg-6">  
@@ -125,7 +125,7 @@
                     <span class="input-group-text" id="basic-addon1"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope icol" viewBox="0 0 16 16">
                       <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/>
                     </svg></span>
-                    <input type="text" class="form-control" placeholder="Madrid" aria-label="Username" aria-describedby="basic-addon1" name="poblacion">
+                    <input id="Poblacion" type="text" class="form-control" placeholder="Madrid" aria-label="Username" aria-describedby="basic-addon1" name="poblacion" required>
                 </div>
               </div>   
             </div> 
@@ -134,7 +134,7 @@
                     <span class="input-group-text" id="basic-addon2"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-key-fill icol" viewBox="0 0 16 16">
                     <path d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5z"/>
                     </svg></span>
-                    <input type="text" class="form-control" placeholder="C/ Imaginaria, nº15, P3, LC" aria-label="Username" aria-describedby="basic-addon1" name="direccion">                    
+                    <input id="direccion" type="text" class="form-control" placeholder="C/ Imaginaria, nº15, P3, LC" aria-label="Username" aria-describedby="basic-addon1" name="direccion" required>                    
                 </div>
             <div class="row">  
               <div class="col-lg-6">
@@ -143,7 +143,7 @@
                     <span class="input-group-text" id="basic-addon2"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-key-fill icol" viewBox="0 0 16 16">
                       <path d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.568 17.568 0 0 0 4.168 6.608 17.569 17.569 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.678.678 0 0 0-.58-.122l-2.19.547a1.745 1.745 0 0 1-1.657-.459L5.482 8.062a1.745 1.745 0 0 1-.46-1.657l.548-2.19a.678.678 0 0 0-.122-.58L3.654 1.328zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"/>
                     </svg></span>
-                    <input type="text" class="form-control" placeholder="910234567" aria-label="Username" aria-describedby="basic-addon1" name="telefonoFijo">     
+                    <input id="tlfFijo" type="text" class="form-control" placeholder="910234567" aria-label="Username" aria-describedby="basic-addon1" name="telefonoFijo" required>     
                 </div>
               </div>  
               <div class="col-lg-6">
@@ -153,7 +153,7 @@
                       <path d="M11 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h6zM5 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H5z"/>
                       <path d="M8 14a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"/>
                     </svg></span>
-                    <input type="text" class="form-control" placeholder="601234567" aria-label="Username" aria-describedby="basic-addon1" name="telefonomovil">
+                    <input id="tlfMovil" type="text" class="form-control" placeholder="601234567" aria-label="Username" aria-describedby="basic-addon1" name="telefonomovil" required>
                 </div>
               </div> 
             </div>       
