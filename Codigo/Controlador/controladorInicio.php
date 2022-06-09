1f6cb1c9c508947a373f0600e325baafcb24edf5
Juan Carlos Tauroni Roldán <juanctauronir@gmail.com>
2022-06-09 19:36:23 +0200

    reseña funciona

--- Codigo/Controlador/controladorInicio.php
+++ Codigo/Controlador/controladorInicio.php
@@ -2,24 +2,20 @@
 session_start();
 include_once 'controladorSesion.php';
 include_once '../Modelo/claseComentario.php';
-    if(isset($_COOKIE['UsuarioLogeado'])){
-        $method = $_SERVER['REQUEST_METHOD'];
-        if ($method =='POST' && validarSesion()) {
-            
-            $nuevoComentario = new Comentario();
+$method = $_SERVER['REQUEST_METHOD'];
 
-            $nuevoComentario->anadirCometario($_POST['nombre'],$_POST['user'],$_POST['comentario']);
-            header('Location: ../index.php');
-        } else {
-            $comentariosMostrados = new Comentario();
-            $comentariosMostrados->obtenerComentarios();
-            include_once '../Vista/paginas/index.php';
-        }
-    } else {
-        cerrarSesion();
-        $comentariosMostrados = new Comentario();
-        $comentariosMostrados->obtenerComentarios();
-        include_once '../Vista/paginas/index.php';
-    }
+if($method == 'POST') {
+    $nuevoComentario = new Comentario();
+
+    $nuevoComentario->anadirCometario($_POST['nombre'],$_POST['user'],$_POST['comentario']);
+    header('Location: ../index.php');
+} else {
+    $comentariosMostrados = new Comentario();
+    $comentariosMostrados->obtenerComentarios();
+    include_once '../Vista/paginas/index.php';
+        
+ 
+}
+
 
 ?>
